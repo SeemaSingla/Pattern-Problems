8623176b3211a9146e97c32bab25c1af851bd824
SeemaSingla <seemasingla7@gmail.com>
2021-09-25 23:36:10 +0530

    Initial commit

---
+++ 1/1.cs
@@ -0,0 +1,13 @@
+int count = 4;
+
+            for (int i = count; i >= 1; i--)
+            {
+
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+
+            }
+            Console.ReadKey();
